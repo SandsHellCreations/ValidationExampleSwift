c3123683d4397051b865691044a7989d1cfdc1c7
Sierra 2 <sierra2@Sierras1.local>
2017-09-14 11:29:29 +0530

    Initial Commit

---
+++ Validations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Validations
+//
+//  Created by Sierra 2 on 14/09/17.
+//  Copyright © 2017 Sierra 2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
