19f5b54a6dbb19dda6d4e4e8b43f49421a673b64
Sandeep Kumar <sandeep.kumar811@gmail.com>
2017-09-15 11:49:58 +0530

    Update Validations.swift

--- Validations/Validations.swift
+++ Validations/Validations.swift
@@ -1,10 +1,10 @@
 //
-//  File.swift
+//  Validations.swift
 //  Validations
 //
 //  Created by Sierra 2 on 14/09/17.
-//  Copyright © 2017 Sierra 2. All rights reserved.
-//
+//  Copyright © SandsHellCreations. All rights reserved.
+//  https://github.com/SandeepSpider811
 
 import Foundation
 
@@ -23,23 +23,32 @@ enum ValidationType {
     case email
     case stringWithFirstLetterCaps
     case phoneNo
+    case alphabeticString
+    case password
 }
 
 enum RegEx: String {
-    case email = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
-    case password = "^.{6,15}$"
-    case alphabeticStringWithSpace = "^[a-zA-Z ]*$"
-    case alphabeticString = "^[A-Z]+[a-zA-Z]*$"
-    case phoneNo = "[0-9]{10,14}"
+    case email = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}" // Email
+    case password = "^.{6,15}$" // Password length 6-15
+    case alphabeticStringWithSpace = "^[a-zA-Z ]*$" // e.g. hello sandeep
+    case alphabeticStringFirstLetterCaps = "^[A-Z]+[a-zA-Z]*$" // SandsHell
+    case phoneNo = "[0-9]{10,14}" // PhoneNo 10-14 Digits
+    
+    //Change RegEx according to your Requirement
 }
 
 enum AlertMessages: String {
     case inValidEmail = "InvalidEmail"
-    case emptyEmail = "Empty Email"
-    case emptyName = "Empty Name"
-    case invalidName = "Invalid Name"
+    case invalidFirstLetterCaps = "First Letter should be capital"
+    case inValidPhone = "Invalid Phone"
+    case invalidAlphabeticString = "Invalid String"
+    case inValidPSW = "Invalid Password"
+    
     case emptyPhone = "Empty Phone"
-    case invalidPhone = "Invalid Phone"
+    case emptyEmail = "Empty Email"
+    case emptyFirstLetterCaps = "Empty Name"
+    case emptyAlphabeticString = "Empty String"
+    case emptyPSW = "Empty Password"
     func localized() -> String {
         return NSLocalizedString(self.rawValue, comment: "")
     }
@@ -53,29 +62,40 @@ class Validation: NSObject {
         for valueToBeChecked in values {
             switch valueToBeChecked.type {
             case .email:
-                if valueToBeChecked.inputValue.isEmpty {
-                    return .failure(.error, .emptyEmail)
-                } else if isValidString(valueToBeChecked.inputValue, .email) != true {
-                    return .failure(.error, .inValidEmail)
+                if let tempValue = isValidString((valueToBeChecked.inputValue, .email, .emptyEmail, .inValidEmail)) {
+                    return tempValue
                 }
             case .stringWithFirstLetterCaps:
-                if valueToBeChecked.inputValue.isEmpty {
-                    return .failure(.error, .emptyName)
-                } else if isValidString(valueToBeChecked.inputValue, .alphabeticString) != true {
-                    return .failure(.error, .invalidName)
+                if let tempValue = isValidString((valueToBeChecked.inputValue, .alphabeticStringFirstLetterCaps, .emptyFirstLetterCaps, .invalidFirstLetterCaps)) {
+                    return tempValue
                 }
             case .phoneNo:
-                if valueToBeChecked.inputValue.isEmpty {
-                    return .failure(.error, .emptyPhone)
-                } else if isValidString(valueToBeChecked.inputValue, .phoneNo) != true {
-                    return .failure(.error, .invalidPhone)
+                if let tempValue = isValidString((valueToBeChecked.inputValue, .phoneNo, .emptyPhone, .inValidPhone)) {
+                    return tempValue
+                }
+            case .alphabeticString:
+                if let tempValue = isValidString((valueToBeChecked.inputValue, .alphabeticStringWithSpace, .emptyAlphabeticString, .invalidAlphabeticString)) {
+                    return tempValue
+                }
+            case .password:
+                if let tempValue = isValidString((valueToBeChecked.inputValue, .password, .emptyPSW, .inValidPSW)) {
+                    return tempValue
                 }
             }
         }
         return .success
     }
     
-    func isValidString(_ testStr: String, _ regex: RegEx) -> Bool {
+    func isValidString(_ input: (text: String, regex: RegEx, emptyAlert: AlertMessages, invalidAlert: AlertMessages)) -> Valid? {
+        if input.text.isEmpty {
+            return .failure(.error, input.emptyAlert)
+        } else if isValidRegEx(input.text, input.regex) != true {
+            return .failure(.error, input.invalidAlert)
+        }
+        return nil
+    }
+    
+    func isValidRegEx(_ testStr: String, _ regex: RegEx) -> Bool {
         let stringTest = NSPredicate(format:"SELF MATCHES %@", regex.rawValue)
         let result = stringTest.evaluate(with: testStr)
         return result
